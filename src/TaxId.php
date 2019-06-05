eb51c4eecb952e301d49b6a7a444dc543594bf0b
traveler4star <traveler4star@gmail.com>
2019-06-05 16:33:29 +0300

    Добавлены новые типы НДС (20, 20/120)

--- src/TaxId.php
+++ src/TaxId.php
@@ -5,6 +5,7 @@ namespace Chekonline\Cashbox;
 
 /**
  * Class TaxId
+ *
  * @package Chekonline\Cashbox
  */
 class TaxId
@@ -13,6 +14,11 @@ class TaxId
      * НДС 18%
      */
     const TAX_18 = 1;
+
+    /**
+     * НДС 20%
+     */
+    const TAX_20 = 1;
     /**
      * НДС 10%
      */
@@ -30,6 +36,10 @@ class TaxId
      */
     const TAX_18_118 = 5;
     /**
+     * Ставка 20/120
+     */
+    const TAX_20_120 = 5;
+    /**
      * Ставка 10/110
      */
     const TAX_10_110 = 6;
@@ -37,12 +47,14 @@ class TaxId
     /**
      * @var array
      */
-    public static $dictionary = array(
+    public static $dictionary = [
         self::TAX_18     => 'НДС 18%',
+        self::TAX_20     => 'НДС 20%',
         self::TAX_10     => 'НДС 10%',
         self::TAX_0      => 'НДС 0%',
         self::TAX_NULL   => 'Без налога',
         self::TAX_18_118 => 'Ставка 18/118',
+        self::TAX_20_120 => 'Ставка 20/120',
         self::TAX_10_110 => 'Ставка 10/110',
-    );
+    ];
 }
